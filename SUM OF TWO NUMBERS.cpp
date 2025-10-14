ef11f528399bbaba1a9f3b5aace2f01f8fb1c47e
Somavo783 <somavodey90@gmail.com>
2025-10-14 07:48:44 +0530

    Add files via upload

---
+++ SUM OF TWO NUMBERS.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+	int a ,b ,sum;
+	printf("ENTER THE VALUE OF a=");
+	scanf("%d", &a);
+	printf("ENTER THE VALUE OF b=");
+	scanf("%d", &b);
+	sum=a+b;
+	printf("THE REQUIRED SUM IS %d", sum);
+	return 0;
+}
