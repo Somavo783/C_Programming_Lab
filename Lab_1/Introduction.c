a855bd93542e366043daa8a8ac520e69489dcfa7
Somavo783 <somavodey90@gmail.com>
2025-10-22 14:40:35 +0530

    Chore: Reorganize files to meet submission requirements

---
+++ Lab_1/Introduction.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+	printf("Name : Somavo Dey\n");
+	printf("Branch : Mechanical Engineering\n");
+	printf("Roll No. : MEB25008\n");
+	printf("Institution : Tezpur University");
+	return 0;
+}
